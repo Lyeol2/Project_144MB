1aaa5df449349b13293abb62c76b7bcb0f823e36
Lyeol2 <frog100f@gmail.com>
2026-05-25 23:07:04 +0900

    feat : Launch.cpp

---
+++ source/core/private/Launch.cpp
@@ -0,0 +1,66 @@
+#include <windows.h>
+
+namespace
+{
+constexpr wchar_t kWindowClassName[] = L"Project144MBLauncherWindow";
+
+LRESULT CALLBACK WindowProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    default:
+        return DefWindowProcW(window, message, wParam, lParam);
+    }
+}
+}
+
+int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int showCommand)
+{
+    WNDCLASSEXW windowClass{};
+    windowClass.cbSize = sizeof(windowClass);
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+    windowClass.lpfnWndProc = WindowProc;
+    windowClass.hInstance = instance;
+    windowClass.hCursor = LoadCursorW(nullptr, IDC_ARROW);
+    windowClass.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
+    windowClass.lpszClassName = kWindowClassName;
+
+    if (!RegisterClassExW(&windowClass))
+    {
+        return 1;
+    }
+
+    HWND window = CreateWindowExW(
+        0,
+        kWindowClassName,
+        L"Project 144MB",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        1280,
+        720,
+        nullptr,
+        nullptr,
+        instance,
+        nullptr);
+
+    if (window == nullptr)
+    {
+        return 1;
+    }
+
+    ShowWindow(window, showCommand);
+    UpdateWindow(window);
+
+    MSG message{};
+    while (GetMessageW(&message, nullptr, 0, 0) > 0)
+    {
+        TranslateMessage(&message);
+        DispatchMessageW(&message);
+    }
+
+    return static_cast<int>(message.wParam);
+}
